152081a47fa930891b2371eb2bb8ec7ad4efd751
solo <113215526+s0lotime@users.noreply.github.com>
2025-05-13 12:29:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import home from './public/index.html'
-
 export default {
   async fetch(request, env, ctx) {
-    return new Response(home, {
+    const url = new URL(request.url);
+    const pathname = url.pathname;
+
+    if (pathname === '/hi') {
+      return new Response(hi, {
       headers: { "Content-Type": "text/html" },
     });
+    }
   },
 };
