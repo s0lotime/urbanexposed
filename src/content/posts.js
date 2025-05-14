e3ca5f79fd331886253751fb7c7f3020b918820f
solo <113215526+s0lotime@users.noreply.github.com>
2025-05-13 20:39:43 -0400

    Content

---
+++ src/content/posts.js
@@ -0,0 +1,25 @@
+export async function contentManagement(request, env, type, user, info) {
+    if (type === 'pic') {
+        if (request === 'POST') {
+            const formData = await request.formData();
+            const file = formData.get("file");
+
+            if (!file || typeof file === "string") {
+                return new Response(JSON.stringify({ success: false, statustext: 'No file attached' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
+            }
+
+            const key = file.name;
+            await env.CDN.put(key, file.stream());
+
+            return new Response(`Uploaded ${key} successfully!`);
+        }
+
+        if (request.method === 'PATCH') {
+            return new Response(JSON.stringify({ success: true, statustext: 'Request is understood, no functionality yet :|' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+        }
+
+        if (request.method === 'DELETE') {
+            return new Response(JSON.stringify({ success: true, statustext: 'Request is understood, no functionality yet :|' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import { contentManagement } from './content/posts.js'
+
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const pathname = url.pathname;
+
+    if (pathname === '/api/content/management') {
+      return contentManagement(request, env, 'pic', 'hi', 'info')
+    }
 
     if (pathname === '/hi') {
       return new Response("hi", {
